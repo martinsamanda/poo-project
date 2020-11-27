04dc73bd7f995782dceabf243f80b3e316d1d263
fabiosaint <fabiofpoli@gmail.com>
2020-11-27 12:12:58 -0300

    Organiza o prototipo
    Renomeia varios arquivos
    Adicona as classes dos blocos quebraveis e não quebraveis
    Adiciona uma classe model para cuidar dos modelos e das instancias
    Adiciona uma classe mapa
    
    Boa tarde do codigo foi inspirado nos tutorias do http://kidscancode.org/lessons/

---
+++ game_view.py
@@ -0,0 +1,36 @@
+import pygame
+
+from settings import *
+
+
+class GameView:
+    def __init__(self, controller, model):
+        self.__GameController = controller
+        self.__GameModel = model
+        self.__screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.__FPS = pygame.time.Clock()
+        self.__font = pygame.font.SysFont("Verdana", 60)
+
+    def starting_settings(self):
+        pygame.display.set_caption(TITLE)
+        self.__screen.fill(WHITE)
+
+    def draw(self):
+        self.__FPS.tick(FPS)
+        self.__screen.fill(WHITE)
+        self.draw_grid()
+        self.__GameModel.all_sprites.draw(self.__screen)
+        pygame.display.flip()
+
+    def draw_grid(self):
+        for x in range(0, SCREEN_WIDTH, TILESIZE):
+            pygame.draw.line(self.__screen, SILVER, (x, 0), (x, SCREEN_HEIGHT))
+        for y in range(0, SCREEN_HEIGHT, TILESIZE):
+            pygame.draw.line(self.__screen, SILVER, (0, y), (SCREEN_WIDTH, y))
+
+    def end(self):
+        game_over = self.__font.render('GAME OVER', True, BLACK)
+        game_over_rect = game_over.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
+        self.__screen.fill(RED)
+        self.__screen.blit(game_over, game_over_rect)
+        pygame.display.flip()
