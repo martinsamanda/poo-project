0d389dc0ec37ca39e9cb6fbada8472b51d2ad01b
WillKruscinscki <wills.kruscinscki@gmail.com>
2020-12-13 04:12:28 -0300

    update e divisão de classes em arquivos separados

--- golem.py
+++ golem.py
@@ -1,3 +1,15 @@
-class Golem:
-    def __init__(self):
-        pass
+import pygame
+from enemy import Enemy
+from character import Character
+from os import path, listdir
+from pygame.locals import *
+from settings import *
+from attack import Attack
+vec = pygame.math.Vector2
+
+
+class Golem(Enemy):
+    def __init__(self, position_x, position_y, model):
+        Character.__init__(self, GOLEM_FOLDER, 30, 85, position_x * TILESIZE, position_y * TILESIZE, model, ENEMY_LAYER)
+        self.direction = 1
+        self.model.enemies.add(self)
